29921a5a775c72e8b1b0bba54755a21599a20f9a
VitorMarquezz <ovitormarquez@gmail.com>
2024-01-02 17:12:59 -0300

    ajustes2

--- src/components/rodape/index.js
+++ src/components/rodape/index.js
@@ -2,9 +2,11 @@ import styles from './Rodape.module.css'
 
 export default function Rodape() {
   return (
-    <footer className={styles.container}>
-      <p className={styles.made}>Feito por Vitor Marquez</p>
-      <p className={styles.email}>ovitormarquez@gmail.com</p>
-    </footer>
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <p className={styles.made}>Feito por Vitor Marquez</p>
+        <p className={styles.email}>ovitormarquez@gmail.com</p>
+      </footer>
+    </div>
   );
 }
