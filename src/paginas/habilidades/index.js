d39baf1831b6d496241b79febd2545af645c27e7
VitorMarquezz <ovitormarquez@gmail.com>
2024-01-08 14:56:36 -0300

    Acredito ser a versão final

--- src/paginas/habilidades/index.js
+++ src/paginas/habilidades/index.js
@@ -1,12 +1,18 @@
 import Footer from "../../components/footer";
 import styles from "./Habilidades.module.css"
+import vectorJS from '../../img/vector-js.png'
+import vectorHTML from '../../img/vector-html.png'
+import vectorCSS from "../../img/vector-css.png";
+import vectorREACTjs from "../../img/vector-reactjs.png";
 
 export default function Habilidades() {
     return (
-      <main>
-        <div>
-          <h1>Michellinha s2
-          </h1>
+      <main className={styles.container}>
+        <div className={styles.images}>
+          <img className={styles.vector} src={vectorHTML} alt="vector javascript" />
+          <img className={styles.vector} src={vectorCSS} alt="vector javascript" />
+          <img className={styles.vector} src={vectorJS} alt="vector javascript" />
+          <img className={styles.vector} src={vectorREACTjs} alt="vector javascript" />
         </div>
         <Footer />
       </main>
